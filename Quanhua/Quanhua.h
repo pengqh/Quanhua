b25cdd19b74ccee5336095550b4c0c56bb368600
pengqh <pengquanhua@yeah.net>
2019-04-12 11:19:58 +0800

    add Quanhua

---
+++ Quanhua/Quanhua.h
@@ -0,0 +1,19 @@
+//
+//  Quanhua.h
+//  Quanhua
+//
+//  Created by pengquanhua on 2019/4/12.
+//  Copyright © 2019年 ifanr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Quanhua.
+FOUNDATION_EXPORT double QuanhuaVersionNumber;
+
+//! Project version string for Quanhua.
+FOUNDATION_EXPORT const unsigned char QuanhuaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Quanhua/PublicHeader.h>
+
+
---
+++ Quanhua/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  Quanhua
+//
+//  Created by pengquanhua on 2019/4/12.
+//  Copyright © 2019年 ifanr. All rights reserved.
+//
+
+import UIKit
+
+class Test: NSObject {
+
+}
